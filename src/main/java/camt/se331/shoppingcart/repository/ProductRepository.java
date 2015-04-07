b6b13cbd47bdf7847b56865c5b807c1a6205fcb5
se552115067 <visava_c@cmu.ac.th>
2015-04-07 21:17:00 +0700

    7.4 completed

--- src/main/java/camt/se331/shoppingcart/repository/ProductRepository.java
+++ src/main/java/camt/se331/shoppingcart/repository/ProductRepository.java
@@ -12,5 +12,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product,Long> {
     List<Product> findByNameLike(String name);
     List<Product> findByNameContaining(String name);
+    List<Product> findByNameContainingIgnoreCase(String name);
+
 }
 
