ecc30973f912bea939ed8fe52b6945fc5e3fb0f3
punjasin <punjasin_p@cmu.ac.th>
2015-04-07 20:01:29 +0700

    7.4 552115039

--- src/main/java/camt/se331/shoppingcart/dao/DbProductDao.java
+++ src/main/java/camt/se331/shoppingcart/dao/DbProductDao.java
@@ -23,7 +23,7 @@ public class DbProductDao implements ProductDao{
 
     @Override
     public List<Product> getProductsByName(String name) {
-        return productRepository.findByNameContaining(name);
+        return productRepository.findByNameContainingIgnoreCase(name);
     }
 
 
